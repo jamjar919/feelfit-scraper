a1fcef6e0d53fa6ba0b15215fe23cbe5b1423edb
James Paterson <jpaterson@tripadvisor.com>
2021-09-06 00:09:04 +0100

    mins

--- src/server/db/getPredictedCountForDayFromDatabase.ts
+++ src/server/db/getPredictedCountForDayFromDatabase.ts
@@ -3,12 +3,67 @@ import {PredictedCountResponse} from "../../common/ApiResponse";
 
 const QUERY = "SELECT timestamp, count FROM `feelfit` WHERE WEEKDAY(DATE(timestamp)) = WEEKDAY(NOW()) ORDER BY `timestamp` DESC"
 
-type Bucket = {
+type Bin = {
     minute: number;
     counts: number[];
 };
 
 const getMinutesSinceStartOfDay = (timestamp: Date) => timestamp.getUTCHours() * 60 + timestamp.getUTCMinutes();
+
+const binResultsPerMinute = (
+    results: { timestamp: string; count: number; }[],
+    minIncrement: number
+): Bin[] => {
+    const parsedResults: {
+        timestamp: Date,
+        count: number
+    }[] = results
+        .map(({ count, timestamp }) => ({ count, timestamp: new Date(timestamp) }))
+        .sort((a, b) =>
+            getMinutesSinceStartOfDay(a.timestamp) < getMinutesSinceStartOfDay(b.timestamp) ? -1 : 1
+        );
+
+    let currentMin = 0;
+    let currentBin: Bin = {
+        minute: currentMin,
+        counts: []
+    };
+
+    const buckets: Bin[] = []
+    parsedResults
+        .forEach(({ count, timestamp }) => {
+            let minutesSinceStartOfDay = getMinutesSinceStartOfDay(timestamp);
+
+            while (
+                (currentMin + minIncrement < minutesSinceStartOfDay)
+            ) {
+                buckets.push(currentBin);
+
+                currentMin += minIncrement;
+                currentBin = {
+                    minute: currentMin,
+                    counts: []
+                };
+            }
+            
+            currentBin.counts.push(count);
+        })
+
+    buckets.push(currentBin);
+
+    return buckets;
+}
+
+const getQuartiles = (counts: number[]) => {
+    const size = counts.length;
+    const sorted = counts.sort((a, b) => a < b ? -1 : 1);
+
+    return [
+        sorted[Math.floor(size * 0.25)],
+        sorted[Math.floor(size * 0.5)],
+        sorted[Math.floor(size * 0.75)],
+    ].map((quartile: number) => typeof quartile === "undefined" ? 0 : quartile);
+};
 
 const getPredictedCountForDayFromDatabase = (handleResults: (results: PredictedCountResponse) => void) => {
 
@@ -16,46 +71,21 @@ const getPredictedCountForDayFromDatabase = (handleResults: (results: PredictedC
         if (error) throw error;
 
         // Bin the results every 15 mins
-        const parsedResults: {
-            timestamp: Date,
-            count: number
-        }[] = results
-            .map(({ count, timestamp }) => ({ count, timestamp: new Date(timestamp) }))
-            .sort((a, b) =>
-                getMinutesSinceStartOfDay(a.timestamp) < getMinutesSinceStartOfDay(b.timestamp) ? -1 : 1
-            );
+        const binnedResults = binResultsPerMinute(results, 15);
 
-        let currentMin = 0;
-        let minIncrement = 15;
-        const buckets: Bucket[] = []
+        // Calculate quartiles and return
+        const resultsWithQuartiles = binnedResults
+            .map(({minute, counts}) => {
+                const hours = Math.floor(minute / 60);
 
-        let currentBucket: Bucket = {
-            minute: currentMin,
-            counts: []
-        };
+                return {
+                    hours,
+                    minute: minute - hours * 60,
+                    quartiles: getQuartiles(counts)
+                };
+            });
 
-        parsedResults
-            .forEach(({ count, timestamp }) => {
-                let minutesSinceStartOfDay = getMinutesSinceStartOfDay(timestamp);
-
-                console.log("minutesSinceStartOfDay", minutesSinceStartOfDay);
-
-                while (
-                    (currentMin < minutesSinceStartOfDay + minIncrement)
-                ) {
-                    buckets.push(currentBucket);
-
-                    currentMin += minIncrement;
-                    currentBucket = {
-                        minute: currentMin,
-                        counts: []
-                    };
-                }
-
-                currentBucket.counts.push(count);
-            })
-
-        handleResults(buckets as any);
+        handleResults(resultsWithQuartiles as any);
     });
 };
 
